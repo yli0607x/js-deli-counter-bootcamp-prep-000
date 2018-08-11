0295ae640571f1ca69229e2c7c77c8fcee9e7bce
Yu Li <yli0607x@gmail.com>
2018-08-11 20:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length==0) {
     return `There is nobody waiting to be served!`}
 
-  return `Currently serving ${katzDeliLine.shift()}.`
+  return `Currently serving ${katzDeliLine}.`
+  katzDeliLine.shift()
 
 
   }
