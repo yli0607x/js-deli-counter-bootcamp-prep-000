4329646281d0f18bd19f9fa0145e1a66c61bd753
Yu Li <yli0607x@gmail.com>
2018-08-11 20:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length==0) {
     return `There is nobody waiting to be served!`}
 
-  return `Currently serving ${katzDeliLine[0]}.`
+  `Currently serving ${katzDeliLine[0]}.`
   katzDeliLine.shift()
 
 
